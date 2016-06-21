27cf924cce143046ce1242e11e62fa25f88387d7
Carsten Klee <cKlee@users.noreply.github.com>
2016-06-21 07:00:27 +0200

    erasing comments

--- lib/Catmandu/Fix/marc_spec.pm
+++ lib/Catmandu/Fix/marc_spec.pm
@@ -1,6 +1,5 @@
 package Catmandu::Fix::marc_spec;
 
-use DDP;
 use Moo;
 use Catmandu::Sane;
 use Catmandu::Util qw(:data);
@@ -32,26 +31,11 @@ sub fix {
     # get MARCspec
     $cache->{$self->spec} = MARC::Spec->new($self->spec) unless(defined $cache->{$self->spec});
     my $ms = $cache->{$self->spec};
-    #p $cache;
-    #p $ms;
-    #p $ms->field;
-    #p $ms->subfields;
-    #p $ms->field->tag;
-    
-    
-    #p $self->spec;
-    #p $self->data;
-    #p $self->path;
-    #p $data;
-    #p $data->{$record_key};
-
 
     my %opts;
     $opts{'-split'}  = $self->split;
     $opts{'-join'}   = $self->join;
     $opts{'-pluck'}  = $self->pluck;
-    #$opts{'-value'}  = $self->value;
-    #$opts{'-record'} = $self->record;
     
     # indicators
     my $indicators = '';
@@ -119,19 +103,14 @@ sub fix {
     if(defined $ms->subfields) { # now we dealing with subfields
         # set the order of subfields
         my @sf_spec =  map { $_ } @{$ms->subfields};
-        #p @sf_spec;
-        #@{$ms->subfields} = sort {$a->code cmp $b->code} @{$ms->subfields}
         @sf_spec = sort {$a->code cmp $b->code} @sf_spec
             unless($self->pluck);
-        #p @sf_spec;
-        #p $ms->subfields;
-        my ($subfields,$subfield,$sf_range,$char_start);
+
+       my ($subfields,$subfield,$sf_range,$char_start);
 
         for my $field (@fields) {
-            
             my $start = (defined $field->[3] && $field->[3] eq '_') ? 5 : 3;
-            
-            #for my $sf (@{$ms->subfields}) {
+
             for my $sf (@sf_spec) {
                 $subfield = [];
                 my $code = $sf->code;
@@ -141,14 +120,14 @@ sub fix {
                     }
                 }
                 next unless(@$subfield);
-    #p $sf->code;
-    #p $subfield;
+
                 # filter by index
                 unless(-1 eq $sf->indexLength) {
                     $sf_range = $get_index_range->($sf, scalar @$subfield);
                     @$subfield = map { defined ${$subfield}[$_] ? ${$subfield}[$_] : () } @$sf_range;
                     next unless(@$subfield);
                 }
+
                 # get substring
                 if(defined $sf->charPos) {
                     $char_start = ('#' eq $sf->charStart) ? $sf->charLength * -1 : $sf->charStart;
@@ -156,11 +135,9 @@ sub fix {
                 }
 
                 push @$subfields, @$subfield if(@$subfield);
-                #p $subfields;
             }
         }
 
-#p $subfields;
         return $data unless($subfields);
 
         my $nested = data_at($path, $data, create => 1, key => $key);
@@ -174,7 +151,6 @@ sub fix {
     } else { # no subfields requested
         my $mapped;
         @$mapped = marc_map($tmp_record, $marc_path, %opts);
-#p $marc_path;
         return $data unless($mapped);
 
         # get substring
@@ -194,7 +170,6 @@ sub fix {
         }
         
     }
-#p $data;
     return $data;
 }
 
@@ -205,29 +180,217 @@ __END__
 
 =head1 NAME
 
-Catmandu::Fix::marc_spec - Blah blah blah
+Catmandu::Fix::marc_spec - reference MARC values via L<MARCspec - A common MARC record path language|http://marcspec.github.io/MARCspec/>
 
 =head1 SYNOPSIS
 
-  use Catmandu::Fix::marc_spec;
+    # Assign value of MARC leader to my.ldr.all
+    marc_spec('LDR', my.ldr.all)
+    
+    # Assign values of all subfields of field 245 as a joined string
+    marc_spec('245', my.title.all)
+    
+    # If field 245 exists, set string 'the title' as the value of my.title.default
+    marc_spec('245', my.title.default, value:'the title')
+    
+    # Assign values of all subfields of every field 650 to my.subjects.all
+    # as a joined string
+    marc_spec('650', my.subjects.all)
+    
+    # Same as above with joining characters '###'
+    marc_spec('650', my.subjects.all, join:'###')
+    
+    # Same as above but added as an element to the array my.append.subjects
+    marc_spec('650', my.append.subjects.$append, join:'###')
+    
+    # Every value of a subfield will be an array element
+    marc_spec('650', my.split.subjects, split:1)
+    
+    # Assign values of all subfields of all fields having indicator 1 = 1
+    # and indicator 2 = 0 to the my.fields.indicators10 array.
+    marc_spec('..._10', my.fields.indicators10.$append)
+    
+    # Assign first four characters of leader to my.firstcharpos.ldr
+    marc_spec('LDR/0-3', my.firstcharpos.ldr)
+    
+    # Assign last four characters of leader to my.lastcharpos.ldr
+    marc_spec('LDR/#-3', my.lastcharpos.ldr)
+    
+    # Assign value of subfield a of field 245 to my.title.proper
+    marc_spec('245$a', my.title.proper)
+    
+    # Assign first two characters of subfield a of field 245 to my.title.proper
+    marc_spec('245$a/0-1', my.title.charpos)
+    
+    # Assign all subfields of second field 650 to my.second.subject
+    marc_spec('650[1]', my.second.subject)
+    
+    # Assign values of all subfields of last field 650 to my.last.subject
+    marc_spec('650[#]', my.last.subject)
+    
+    # Assign an array of values of all subfields of the first two fields 650
+    # to my.two.split.subjects
+    marc_spec('650[0-1]', my.two.split.subjects, split:1)
+    
+    # Assign a joined string of values of all subfields of the last two fields 650
+    # to my.two.join.subjects
+    marc_spec('650[#-1]', my.two.join.subjects, join:'###')
+    
+    
+    # Assign value of first subfield a of all fields 020 to my.isbn.number
+    marc_spec('020$a[0]', my.isbn.number)
+    
+    # Assign value of first subfield q of first field 020 to my.isbn.qual.one
+    marc_spec('020[0]$q[0]', my.isbn.qual.none)
+    
+    # Assign values of subfield q and a in the order stated as an array
+    # to  my.isbns.pluck.all
+    # without option 'pluck:1' the elments will be in 'natural' order
+    # see example below
+    marc_spec('020$q$a', my.isbns.pluck.all, split:1, pluck:1)
+    
+    # Assign value of last subfield q and second subfield a 
+    # in 'natural' order of last field 020 as an array to my.isbn.qual.other
+    marc_spec('020[#]$q[#]$a[1]', my.isbn.qual.other, split:1)
+    
+    # Assign first five characters of value of last subfield q and last character
+    # of value of second subfield a in 'natural' order of all fields 020
+    # as an array to  my.isbn.qual.substring.other
+    marc_spec('020$q[#]/0-4$a[1]/#', my.isbn.qual.substring.other, split:1)
 
 =head1 DESCRIPTION
 
-Catmandu::Fix::marc_spec is
+L<Catmandu::Fix::marc_spec|Catmandu::Fix::marc_spec> is a fix method for the 
+famous L<Catmandu Framework|Catmandu>.
+
+It behaves like <Catmandu::Fix::marc_map|Catmandu::Fix::marc_map> for the most
+part, but has a more fine grained method to reference data content.
+
+See L<MARCspec - A common MARC record path language|http://marcspec.github.io/MARCspec/> 
+for documentation on the path syntax.
+
+=head1 METHODS
+
+=head2 marc_spec($marcspec, $var, %options)
+
+$marcspec is a string with the syntax of
+L<MARCspec - A common MARC record path language|http://marcspec.github.io/MARCspec/>.
+Use always single quotes with this first parameter.
+
+$var is the variable to assign referenced values to. Use $var.$append to
+add referenced data values as an array element.
+
+    # INPUT
+    [245,1,0,"a","Cross-platform Perl /","c","Eric F. Johnson."]
+    
+    # CALL
+    marc_spec('245', my.title.$append)
+    
+    # OUTPUT
+    ["Cross-platform Perl /Eric F. Johnson."]
+
+
+=head1 OPTIONS
+
+=head2 split
+
+If split is set to 1, every fixed fields value or every subfield will be
+an array element.
+
+    # INPUT
+    [650," ",0,"a","Perl (Computer program language)"],
+    [650," ",0,"a","Web servers."]
+    
+    # CALL
+    marc_spec('650', my.split.subjects, split:1)
+    
+    # OUTPUT
+    ["Perl (Computer program language)", "Web servers."]
+
+=head2 join
+
+If set, value of join will be used to join the referenced data content.
+This will only have an effect if option split is undefined (not set or set to 0).
+
+    # INPUT
+    [650," ",0,"a","Perl (Computer program language)"],
+    [650," ",0,"a","Web servers."]
+    
+    # CALL
+    marc_spec('650', my.join.subjects, join:'###')
+    
+    # OUTPUT
+    "Perl (Computer program language)###Web servers."
+
+=head2 pluck
+
+This has only an effect on subfield values. By default subfield reference
+happens in 'natural' order (first number 0 to 9 and then letters a to z).
+
+    # INPUT
+    ["020"," ", " ","a","0491001304","q","black leather"]
+    
+    # CALL
+    marc_spec('020$q$a', my.natural.isbn, split:1)
+    
+    # OUTPUT
+    [0491001304, "black leather"]
+    
+
+If pluck is set to 1, values will be referenced by the order stated in the
+MARCspec.
+
+    # INPUT
+    ["020"," ", " ","a","0491001304","q","black leather"]
+    
+    # CALL
+    marc_spec('020$q$a', my.natural.isbn, split:1, pluck:1)
+    
+    # OUTPUT
+    ["black leather", 0491001304]
+
+=head2 value
+
+If set to an value, this value will be assigned to $var if MARCspec references
+data content (if the field or subfield exists). 
+
+=head2 record
+
+The value of option record is used as a record key. Thus not the default record,
+but the other record will be processed.
+
+This option is useful if you created another (temporary) record and want to
+work on this record instead of the default record.
+
+    copy_field(record, record2)
+    # do some stuff with record2 an later
+    marc_spec('245$a', my.title.other, record:'record2')
 
 =head1 AUTHOR
 
 Carsten Klee E<lt>klee@cpan.orgE<gt>
 
-=head1 COPYRIGHT
+=head1 CONTRIBUTORS
 
-Copyright 2016- Carsten Klee
+=over
 
-=head1 LICENSE
+=item * Johann Rolschewski, C<< <jorol at cpan> >>,
+
+=item * Patrick Hochstenbach, C<< <patrick.hochstenbach at ugent.be> >>,
+
+=item * Nicolas Steenlant, C<< <nicolas.steenlant at ugent.be> >>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 SEE ALSO
+
+L<Catmandu::Fix|Catmandu::Fix>,
+L<Catmandu::MARC|Catmandu::MARC>,
+L<Catmandu::MARC::Fix::marc_map|Catmandu::MARC::Fix::marc_map>
 
 =cut
